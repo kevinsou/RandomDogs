246589af1e8f5844e2b4e786f9743cce4693ffd6
kevinsou <kevinsou.ks@gmail.com>
2018-12-18 17:55:08 -0800

    new changes

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
-const dog={
-    url=''
-};
+const dogUrl=[
+    'https://random.dog/woof.json'
+]
+const generateBtn = document.querySelector("button");
 
-generateDog(){
-    fetch('https://random.dog/woof.json')
-        .then(response=>{return response.json();})
-        .then(result=>{console.log(response)})
+var dogImage=document.querySelector("img");
+
+var urlImage='';
+
+function generate(){
+    Promise.all(dogUrl.map(url =>{
+        return fetch(url).then(resp=>resp.json())
+    })).then(results =>{
+        urlImage=results;
+        dogImage.src=urlImage[0].url;
+    }).catch(()=>console.log('error'))
 }
+
+Promise.all(dogUrl.map(url =>{
+    return fetch(url).then(resp=>resp.json())
+})).then(results =>{
+    urlImage=results;
+    dogImage.src=urlImage[0].url;
+}).catch(()=>console.log('error'))
+
+
+generateBtn.addEventListener("click",generate);
 
